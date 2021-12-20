e53a3efc65ec139038637135bc20f0e687d03e86
Martti Malmi <sirius@iki.fi>
2021-12-20 16:20:57 +0200

    8MB max msg size

--- src/main.rs
+++ src/main.rs
@@ -40,8 +40,8 @@ async fn main() {
             // This will call our function if the handshake succeeds.
             ws
                 .max_send_queue(1)
-                .max_message_size(5 * 1000 * 1000)
-                .max_frame_size(5 * 1000 * 1000)
+                .max_message_size(8 * 1000 * 1000)
+                .max_frame_size(8 * 1000 * 1000)
                 .on_upgrade(move |socket| user_connected(socket, users))
         });
 
